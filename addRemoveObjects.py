05ce03938ac15919f691fb2ba0b2831d4defc6cc
nikemr <nkemer@gmail.com>
2020-06-13 23:40:15 +0300

    some bad code removed- (removing nodes)

--- addRemoveObjects.py
+++ addRemoveObjects.py
@@ -1,4 +1,3 @@
-
 import torch
 
 import numpy as np
@@ -48,29 +47,29 @@ import lilly_11
 # import ernie
 import time
 
+base = ShowBase()
 
-
-
-
-
-base=ShowBase()
 
 class Yer(DirectObject):
-    def __init__(self):
+
+    def __init__(self):   
+
+        self.agent_name='agent0'
+        self.agent_number=0
         gltf.patch_loader(base.loader)
         # create a rendering window
-        wp=WindowProperties()
-        wp.setSize(1000,1000)
-        base.win.requestProperties(wp) 
+        wp = WindowProperties()
+        wp.setSize(1000, 1000)
+        base.win.requestProperties(wp)
         base.setBackgroundColor(0.1, 0.1, 0.8, 1)
-        base.setFrameRateMeter(True)        
+        base.setFrameRateMeter(True)
         base.cam.setPos(0, -80, 50)
-        base.cam.lookAt(0, 0, 0) 
+        base.cam.lookAt(0, 0, 0)
         base.disableMouse()
         base.useTrackball()
 
         # Light      ####################################################################################
-        
+
         alight = AmbientLight('ambientLight')
         alight.setColor(Vec4(0.5, 0.5, 0.5, 1))
         alightNP = render.attachNewNode(alight)
@@ -83,15 +82,19 @@ class Yer(DirectObject):
         render.setLight(dlightNP)
         render.setShaderAuto()
 
+        self.landscape()
 
-             
-        
+        taskMgr.add(self.update, 'updateWorld')
 
-        self.t=1
-
-        self.landscape() 
-        taskMgr.add(self.update, 'updateWorld')        
         self.accept('f3', self.toggleDebug)
+
+
+
+
+        # remove a agent
+        self.accept('k',self.agent_trig,[self.remove_agent])
+        # add an agent
+        self.accept('n', self.agent_trig,[self.make_agent])
 
         # inputState.watchWithModifiers('forward', 'w')
         # inputState.watchWithModifiers('left', 'a')
@@ -101,9 +104,9 @@ class Yer(DirectObject):
         # inputState.watchWithModifiers('turnRight', 'e')
         # inputState.watchWithModifiers('pulse', 'p')
         # inputState.watchWithModifiers('jump', 'j')
-        inputState.watchWithModifiers('removeNode', 'k')
+        # inputState.watchWithModifiers('removeNode', 'k')
 
-        
+
 
     def toggleDebug(self):
         if self.debugNP.isHidden():
@@ -111,17 +114,16 @@ class Yer(DirectObject):
         else:
             self.debugNP.hide()
 
-
     def landscape(self):
         ## Bullet World #################################################################################
         self.worldNP = render.attachNewNode('World')
-        
+
         self.debugNP = self.worldNP.attachNewNode(BulletDebugNode('Debug'))
         self.debugNP.show()
         self.debugNP.node().showNormals(True)
 
         self.world = BulletWorld()
-        self.world.setDebugNode(self.debugNP.node())        
+        self.world.setDebugNode(self.debugNP.node())
         self.world.setGravity(Vec3(0, 0, -9.81))
 
         # Heightfield Surface ##########################################################################
@@ -133,100 +135,99 @@ class Yer(DirectObject):
         shape.setUseDiamondSubdivision(True)
         np = self.worldNP.attachNewNode(BulletRigidBodyNode('Heightfield'))
         np.node().addShape(shape)
-        np.setPos(0, 0, 0)        
+        np.setPos(0, 0, 0)
         np.set_scale(2)
         np.node().setFriction(.5)
         np.setCollideMask(BitMask32.allOn())
         self.world.attach(np.node())
-        self.hf = np.node() # To enable/disable debug visualisation
+        self.hf = np.node()  # To enable/disable debug visualisation
         self.terrain = GeoMipTerrain('terrain')
-        self.terrain.setHeightfield(img)    
+        self.terrain.setHeightfield(img)
         self.terrain.setBlockSize(32)
-        #I don't want any optimization that's why I commented that
-        #self.terrain.setNear(50)
-        #self.terrain.setFar(100)
-        #self.terrain.setFocalPoint(base.camera)    
+        # I don't want any optimization that's why I commented that
+        # self.terrain.setNear(50)
+        # self.terrain.setFar(100)
+        # self.terrain.setFocalPoint(base.camera)
         rootNP = self.terrain.getRoot()
         rootNP.reparentTo(render)
-        rootNP.setSz(height*2)
+        rootNP.setSz(height * 2)
         rootNP.setSx(2)
         rootNP.setSy(2)
         offset = img.getXSize() / 2.0 - 0.5
-        rootNP.setPos(-offset*2, -offset*2, -height )    
+        rootNP.setPos(-offset * 2, -offset * 2, -height)
         self.terrain.generate()
-
-        
-      
-    def processInput(self, dt):
-         
-        if inputState.isSet('removeNode'):            
-            if self.t:
-                self.t=0
-                print(type(self.worldNP.find("m*").node()))
-                print(self.worldNP.find("m*").node())
-                print(self.world)
-                self.world.remove(self.worldNP.find("m*").node())
-                self.worldNP.find("m*").detachNode()                
-                print(f'dolu mu:   {self.worldNP.find("m*")}')
-             
-
 
     def update(self, task):
         dt = globalClock.getDt()
-        self.processInput(dt)
-        self.world.doPhysics(dt) 
+        self.world.doPhysics(dt)
         return task.cont
 
-    def agent_maker(self):
-        '''creates agents for the world'''
+    def remove_agent(self,remove_this):
+
+        print(self.worldNP.find(remove_this))
+        
+        if not self.worldNP.find(remove_this).isEmpty():
+            # print(f'Node type:{type(self.worldNP.find(remove_this).node())}')
+            # print(f'Pybullet Node:{self.worldNP.find(remove_this).node()}')
+            # print(f'Pybullet World{self.world}')
+            self.world.remove(self.worldNP.find(remove_this).node())
+            self.worldNP.find(remove_this).detachNode()
+
+
+
+
+   
+    def agent_trig(self,fn):
+        return fn(self.agent_name)
+
+
+    def make_agent(self,agent_name):
+        """creates agents for the world"""
         shape = BulletCapsuleShape(.35, 1, ZUp)
         shape2 = BulletCapsuleShape(.35, 1, ZUp)
         head = BulletSphereShape(.3)
-        #nodepath---------------------------
-        agent_node_path = self.worldNP.attachNewNode(BulletRigidBodyNode('maked_agent'))
+        # nodepath---------------------------
+        agent_node_path = self.worldNP.attachNewNode(BulletRigidBodyNode(agent_name))
         print(agent_node_path)
         agent_node_path.setPos(0, 0, 10)
         # agent_node_path.set_scale(3)
         agent_node_path.setCollideMask(BitMask32.allOn())
-        #node-------------------------------
+        # node-------------------------------
         agent_node = agent_node_path.node()
         agent_node.setMass(5)
-        agent_node.addShape(shape,TransformState.makePosHpr(Point3(-.35, 0, 0),Point3(90, 0, 90)))
-        agent_node.addShape(shape2,TransformState.makePosHpr(Point3(.35, 0, 0),Point3(90,0, 90)))
-        agent_node.addShape(head,TransformState.makePos(Point3(0, .5, 1)))        
-        agent_node.setFriction(0.5)       
-        #visual representation--------------
+        agent_node.addShape(shape, TransformState.makePosHpr(Point3(-.35, 0, 0), Point3(90, 0, 90)))
+        agent_node.addShape(shape2, TransformState.makePosHpr(Point3(.35, 0, 0), Point3(90, 0, 90)))
+        agent_node.addShape(head, TransformState.makePos(Point3(0, .5, 1)))
+        agent_node.setFriction(0.5)
+        # visual representation--------------
         visualNP = loader.loadModel('models/lilly.gltf')
-        materials=visualNP.findAllMaterials()
+        materials = visualNP.findAllMaterials()
         materials[0].clearBaseColor()
         visualNP.reparentTo(agent_node_path)
+
+        self.agent_number += 1
+        self.agent_name='agent'+str(self.agent_number)
+
         return self.add_to_world(agent_node)
-            
+
+    # def total_agents(self):
 
     def food_maker(self):
-        '''creates food for the world'''
+        """creates food for the world"""
         pass
 
-    
-    def add_to_world(self,add_this):
-        '''adds object to the world'''
-        
+    def add_to_world(self, add_this):
+        """adds object to the world"""
+
         return self.world.attach(add_this)
+
 
 yer = Yer()
 
-
-
-
-
-
-ajan= yer.agent_maker()  
-
-
+ajan = yer.make_agent("agent0")
 
 print("WorldNP ALL Children")
 print(yer.worldNP.getChildren())
-
 
 print(render.getChildren())
 
@@ -236,7 +237,7 @@ print(base.camList)
 print("WORLD NP")
 print(yer.worldNP.findAllMatches("*"))
 
-print("A.J.A.N")  
-print(yer.worldNP.find("m*").node())
-    
-base.run()           
+print("A.J.A.N")
+print(yer.worldNP.find("*").node())
+
+base.run()
